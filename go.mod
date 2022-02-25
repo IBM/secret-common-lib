1be640dd2c6a79641146311089e7da1c7620ec1e
Guna K Kambalimath <Guna.Kambalimath@ibm.com>
2022-02-25 10:06:17 +0530

    Removing watcher

--- go.mod
+++ go.mod
@@ -6,8 +6,6 @@ require (
 	github.com/IBM/secret-utils-lib v0.0.0-20220223110022-899e9e8c74e0
 	go.uber.org/zap v1.20.0
 	google.golang.org/grpc v1.27.1
-	k8s.io/apimachinery v0.21.0
-	k8s.io/client-go v0.0.0-00010101000000-000000000000
 )
 
 replace (
--- pkg/secret_provider/managed_secret_provider.go
+++ pkg/secret_provider/managed_secret_provider.go
@@ -19,19 +19,13 @@ package secret_provider
 import (
 	"context"
 	"flag"
-	"io/ioutil"
 	"net"
 	"time"
 
-	"github.com/IBM/secret-utils-lib/pkg/token"
 	"github.com/IBM/secret-utils-lib/pkg/utils"
 	sp "github.com/IBM/secret-utils-lib/secretprovider"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/watch"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
 )
 
 var (
@@ -41,15 +35,8 @@ var (
 // ManagedSecretProvider ...
 type ManagedSecretProvider struct {
 	logger             *zap.Logger
-	defaultSecretToken string
-	watcher            *secretWatcher
 }
 
-type secretWatcher struct {
-	watcher          watch.Interface
-	secretname       string
-	isUpdateRequired bool
-}
 
 // newManagedSecretProvider ...
 func newManagedSecretProvider(logger *zap.Logger) (*ManagedSecretProvider, error) {
@@ -63,15 +50,8 @@ func newManagedSecretProvider(logger *zap.Logger) (*ManagedSecretProvider, error
 		return nil, utils.Error{Description: "Error establishing grpc connection", BackendError: err.Error()}
 	}
 
-	watcher, err := initSecretWatcher(logger)
-	if err != nil {
-		logger.Error("Error initializing secret watcher")
-		return nil, err
-	}
-	msp := &ManagedSecretProvider{logger: logger, watcher: watcher}
-	go checkUpdate(msp)
 	logger.Info("Initialized managed secret provider")
-	return msp, nil
+	return &ManagedSecretProvider{logger: logger}, nil
 }
 
 // GetDefaultIAMToken ...
@@ -80,16 +60,6 @@ func (msp *ManagedSecretProvider) GetDefaultIAMToken(freshTokenRequired bool) (s
 
 	var tokenlifetime uint64
 
-	if !msp.watcher.isUpdateRequired {
-		// If the token in cache is valid, secret sidecar will not be called
-		tokenlifetime, err := token.CheckTokenLifeTime(msp.defaultSecretToken)
-		if err == nil {
-			msp.logger.Info("Successfully fetched iam token")
-			return msp.defaultSecretToken, tokenlifetime, nil
-		}
-	}
-
-	// token in the cache isn't valid, hence sidecar needs to be called
 	// Connecting to sidecar
 	msp.logger.Info("Connecting to sidecar")
 	conn, err := grpc.Dial(*endpoint, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithDialer(unixConnect))
@@ -103,15 +73,13 @@ func (msp *ManagedSecretProvider) GetDefaultIAMToken(freshTokenRequired bool) (s
 	defer cancel()
 	defer conn.Close()
 
-	response, err := c.GetDefaultIAMToken(ctx, &sp.Request{IsFreshTokenRequired: true, ReadSecret: msp.watcher.isUpdateRequired})
+	response, err := c.GetDefaultIAMToken(ctx, &sp.Request{IsFreshTokenRequired: freshTokenRequired})
 	if err != nil {
 		msp.logger.Error("Error fetching IAM token", zap.Error(err))
 		return "", tokenlifetime, err
 	}
-	msp.watcher.isUpdateRequired = false
+
 	msp.logger.Info("Successfully fetched IAM token for default secret")
-	// Updating the cache with the new token received from sidecar
-	msp.defaultSecretToken = response.Iamtoken
 	return response.Iamtoken, response.Tokenlifetime, nil
 }
 
@@ -151,73 +119,4 @@ func unixConnect(addr string, t time.Duration) (net.Conn, error) {
 	}
 	conn, err := net.DialUnix("unix", nil, unixAddr)
 	return conn, err
-}
-
-// initSecretWatcher ...
-func initSecretWatcher(logger *zap.Logger) (*secretWatcher, error) {
-	logger.Info("Initializing secret watcher")
-
-	// Fetching cluster config used to create k8s client
-	k8sConfig, err := rest.InClusterConfig()
-	if err != nil {
-		logger.Error("Error fetching in cluster config", zap.Error(err))
-		return nil, utils.Error{Description: "Error initiliazing secret watcher", BackendError: err.Error()}
-	}
-
-	// Creating k8s client used to read secret
-	clientset, err := kubernetes.NewForConfig(k8sConfig)
-	if err != nil {
-		logger.Error("Error creating k8s client", zap.Error(err))
-		return nil, utils.Error{Description: "Error initiliazing secret watcher", BackendError: err.Error()}
-	}
-
-	// Reading the namespace in which the pod is deployed
-	byteData, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace")
-	if err != nil {
-		logger.Error("Error fetching namespace", zap.Error(err))
-		return nil, utils.Error{Description: "Error initiliazing secret watcher - unable to read namespace", BackendError: err.Error()}
-	}
-
-	namespace := string(byteData)
-	if namespace == "" {
-		logger.Error("Unable to fetch namespace", zap.Error(err))
-		return nil, utils.Error{Description: "Error initiliazing secret watcher - unable to read namespace"}
-	}
-
-	secretname := utils.IBMCLOUD_CREDENTIALS_SECRET
-	watcher, err := clientset.CoreV1().Secrets(namespace).Watch(context.TODO(), metav1.SingleObject(metav1.ObjectMeta{Name: utils.IBMCLOUD_CREDENTIALS_SECRET, Namespace: namespace}))
-	if err != nil {
-		logger.Error("Error initializing watcher for ibm-cloud-credentials", zap.Error(err))
-		logger.Info("Initializing watcher for storage-secret-store")
-		watcher, err = clientset.CoreV1().Secrets(namespace).Watch(context.TODO(), metav1.SingleObject(metav1.ObjectMeta{Name: utils.STORAGE_SECRET_STORE_SECRET, Namespace: namespace}))
-		if err != nil {
-			logger.Error("Error initializing watcher for storage-secret-store", zap.Error(err))
-			return nil, utils.Error{Description: "Error initiliazing secret watcher", BackendError: err.Error()}
-		}
-		secretname = utils.STORAGE_SECRET_STORE_SECRET
-	}
-
-	logger.Info("Initialized secret watcher")
-	return &secretWatcher{watcher: watcher, secretname: secretname}, nil
-}
-
-// checkUpdate ...
-func checkUpdate(msp *ManagedSecretProvider) {
-	var err error
-	for {
-		event, open := <-msp.watcher.watcher.ResultChan()
-		if open {
-			if event.Type == watch.Deleted {
-				msp.logger.Info("Secret is deleted", zap.String("secret-name", msp.watcher.secretname))
-				msp.watcher, err = initSecretWatcher(msp.logger)
-				if err != nil {
-					msp.logger.Error("Error reinitializing secret, exiting watcher", zap.Error(err))
-					return
-				}
-			} else if event.Type == watch.Modified {
-				msp.logger.Info("Secret is modified", zap.String("secret-name", msp.watcher.secretname))
-			}
-			msp.watcher.isUpdateRequired = true
-		}
-	}
 }
